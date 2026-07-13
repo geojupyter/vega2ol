a9d4a8899f130b58ddd53f9dc5ee17cb46031638
Nakul <nakulverma.py@gmail.com>
2026-07-13 14:06:27 +0530

    v1.2.0

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,11 @@
  * vega2ol - Vega expression to OpenLayers expression transpiler
  */
 
-export { vega2ol, Vega2OLError, VegaNode } from './main.js';
+export {
+  vega2ol,
+  Vega2OLError,
+  VegaNode,
+  FUNCTION_MAPPING,
+  CONSTANTS_MAPPING,
+  OPERATOR_MAPPING,
+} from "./main.js";
